3011bc18d593218535cc69bf8d3598c6693520f2
Lutz Sebastian <general.of.omega@googlemail.com>
2019-02-18 23:09:48 +0100

    Zweiter Versuch 	neue Datei:     .eslintrc.json 	neue Datei:     .github/ISSUE_TEMPLATE/bug_report.md 	neue Datei:     .gitignore 	neue Datei:     .npmignore 	neue Datei:     .travis.yml 	neue Datei:     LICENSE 	neue Datei:     README.md 	neue Datei:     admin/admin.d.ts 	neue Datei:     admin/custom_m.html 	neue Datei:     admin/index_m.html 	neue Datei:     admin/style.css 	neue Datei:     admin/tsconfig.json 	neue Datei:     admin/virtualpowermeter.png 	neue Datei:     admin/words.js 	neue Datei:     gulpfile.js 	neue Datei:     io-package.json 	neue Datei:     lib/adapter-config.d.ts 	neue Datei:     lib/tools.js 	neue Datei:     main.js 	neue Datei:     main.test.js 	neue Datei:     package-lock.json 	neue Datei:     package.json 	neue Datei:     test/integration.js 	neue Datei:     test/mocha.custom.opts 	neue Datei:     test/mocha.setup.js 	neue Datei:     test/package.js 	neue Datei:     test/tsconfig.json 	neue Datei:     test/unit.js 	neue Datei:     tsconfig.json

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"virtualpowermeter adapter settings": {
+		"en": "Adapter settings for virtualpowermeter",
+		"de": "Adaptereinstellungen für virtualpowermeter",
+		"ru": "Настройки адаптера для virtualpowermeter",
+		"pt": "Configurações do adaptador para virtualpowermeter",
+		"nl": "Adapterinstellingen voor virtualpowermeter",
+		"fr": "Paramètres d'adaptateur pour virtualpowermeter",
+		"it": "Impostazioni dell'adattatore per virtualpowermeter",
+		"es": "Ajustes del adaptador para virtualpowermeter",
+		"pl": "Ustawienia adaptera dla virtualpowermeter",
+		"zh-cn": "virtualpowermeter的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
