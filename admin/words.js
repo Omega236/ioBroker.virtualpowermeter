3e38fb96d470c7636a39d17b75ff0d5dfcc23a46
Omega <general.of.omega@googlemail.com>
2020-04-15 17:29:06 +0200

    1.2.1 translation

--- admin/words.js
+++ admin/words.js
@@ -14,36 +14,100 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla virtualpowermeter",
 		"zh-cn": "virtualpowermeter的适配器设置"
 	},
-	"The adapter is configured via the data points of the energy consumers (for example, light.level)": {
-		"en": "The adapter is configured via the data points of the energy consumers (for example, light.level)",
-		"de": "Der Adapter wird über die Datenpunkte der Energieverbraucher (zum Beispiel light.level) konfiguriert"
-	},
-	"Maximum consumption in watts when device is switched on or at 100%": {
-		"en": "Maximum consumption in watts when device is switched on or at 100%",
-		"de": "Maximaler Verbrauch in Watt, wenn Gerät Angeschalten bzw auf 100%"
-	},
-	"enabled": {
-		"en": "enabled",
-		"de": "Aktiv"
-	},
-	"Group name (calculated consumption of the whole group)": {
-		"en": "Group name (calculated consumption of the whole group)",
-		"de": "Gruppenname (errechnet Verbrauch der ganzen Gruppe)"
-	},
-	"Datapointname for Power": {
-		"en": "Datapointname for Power (Watt)",
-		"de": "Datenpunktname für Power (Watt)"
-	},
-	"Datapointname for Energy": {
-		"en": "Datapointname for Energy (Wh)",
-		"de": "Datenpunktname für Energy (Wh)"
-	},
+  "The adapter is configured via the data points of the energy consumers (for example, light.level)": {
+    "en": "The adapter is configured via the data points of the energy consumers (for example, light.level)",
+    "de": "Der Adapter wird über die Datenpunkte der Energieverbraucher konfiguriert (z. B. light.level).",
+    "ru": "Адаптер настраивается через точки данных потребителей энергии (например, light.level)",
+    "pt": "O adaptador é configurado através dos pontos de dados dos consumidores de energia (por exemplo, light.level)",
+    "nl": "De adapter wordt geconfigureerd via de datapunten van de energieverbruikers (bijvoorbeeld light.level)",
+    "fr": "L'adaptateur est configuré via les points de données des consommateurs d'énergie (par exemple, light.level)",
+    "it": "L'adattatore è configurato tramite i punti dati dei consumatori di energia (ad esempio light.level)",
+    "es": "El adaptador se configura a través de los puntos de datos de los consumidores de energía (por ejemplo, light.level)",
+    "pl": "Adapter jest konfigurowany za pomocą punktów danych odbiorców energii (na przykład light.level)",
+    "zh-cn": "通过能量消耗器的数据点（例如，light.level）配置适配器"
+  },
+  "Maximum consumption in watts when device is switched on or at 100%": {
+    "en": "Maximum consumption in watts when device is switched on or at 100%",
+    "de": "Maximaler Verbrauch in Watt bei eingeschaltetem Gerät oder bei 100%",
+    "ru": "Максимальное потребление в ваттах при включении устройства или при 100%",
+    "pt": "Consumo máximo em watts quando o dispositivo está ligado ou a 100%",
+    "nl": "Maximaal verbruik in watt wanneer het apparaat is ingeschakeld of op 100%",
+    "fr": "Consommation maximale en watts lorsque l'appareil est allumé ou à 100%",
+    "it": "Consumo massimo in watt all'accensione del dispositivo o al 100%",
+    "es": "Consumo máximo en vatios cuando el dispositivo está encendido o al 100%",
+    "pl": "Maksymalne zużycie w watach przy włączonym urządzeniu lub przy 100%",
+    "zh-cn": "设备开机或100％时的最大功耗（瓦）"
+  },
+  "enabled": {
+    "en": "enabled",
+    "de": "aktiviert",
+    "ru": "включен",
+    "pt": "ativado",
+    "nl": "ingeschakeld",
+    "fr": "activée",
+    "it": "abilitato",
+    "es": "habilitado",
+    "pl": "włączone",
+    "zh-cn": "已启用"
+  },
+  "Group name (calculated consumption of the whole group)": {
+    "en": "Group name (calculated consumption of the whole group)",
+    "de": "Gruppenname (berechneter Verbrauch der gesamten Gruppe)",
+    "ru": "Название группы (расчетное потребление всей группы)",
+    "pt": "Nome do grupo (consumo calculado de todo o grupo)",
+    "nl": "Groepsnaam (berekend verbruik van de hele groep)",
+    "fr": "Nom du groupe (consommation calculée de l'ensemble du groupe)",
+    "it": "Nome del gruppo (consumo calcolato dell'intero gruppo)",
+    "es": "Nombre del grupo (consumo calculado de todo el grupo)",
+    "pl": "Nazwa grupy (obliczone zużycie dla całej grupy)",
+    "zh-cn": "组名（整个组的计算消耗量）"
+  },
+  "Datapointname for Power": {
+    "en": "Datapointname for Power",
+    "de": "Datenpunktname für Power",
+    "ru": "Datapointname для власти",
+    "pt": "Datapointname for Power",
+    "nl": "Datapuntnaam voor Power",
+    "fr": "Nom du point de données pour l'alimentation",
+    "it": "Datapointname for Power",
+    "es": "Nombre del punto de datos para Power",
+    "pl": "Nazwa punktu danych dla mocy",
+    "zh-cn": "电源的数据点名称"
+  },
+  "Datapointname for Energy": {
+    "en": "Datapointname for Energy",
+    "de": "Datenpunktname für Energie",
+    "ru": "Datapointname для энергии",
+    "pt": "Datapointname for Energy",
+    "nl": "Datapuntnaam voor energie",
+    "fr": "Nom de point de données pour l'énergie",
+    "it": "Datapointname for Energy",
+    "es": "Nombre de punto de datos para energía",
+    "pl": "Nazwa punktu danych dla energii",
+    "zh-cn": "能源的数据点名称"
+  },
 	"inverted": {
-		"en": "inverted",
-		"de": "Invertiert"
-	},
-	"use inverted value. if datapoint is 'SwitchedOff' you can inverted so it Calculates the Power on false and not on true" : {
-		"en": "use inverted value. if datapoint is 'SwitchedOff' you can inverted so it Calculates the Power on false and not on true",
-		"de": "Arbeitet invertiert. Wenn man einen Datenpunkt 'IstAusgeschalten' hat kann man diesen invertieren, dadurch wird bei false die Power berechnet, bei true nicht"
-	}
+    "en": "inverted",
+    "de": "invertiert",
+    "ru": "перевернутый",
+    "pt": "invertido",
+    "nl": "omgekeerd",
+    "fr": "inversé",
+    "it": "rovesciato",
+    "es": "invertido",
+    "pl": "odwrotny",
+    "zh-cn": "倒"
+  },
+  "use inverted value. if datapoint is 'SwitchedOff' you can inverted so it Calculates the Power on false and not on true": {
+    "en": "use inverted value. if datapoint is 'SwitchedOff' you can inverted so it Calculates the Power on false and not on true",
+    "de": "Verwenden Sie einen invertierten Wert. Wenn der Datenpunkt 'Abgeschalten' ist, können Sie ihn invertieren, um die Leistung bei falsch und nicht bei wahr zu berechnen",
+    "ru": "используйте инвертированное значение. если datapoint имеет значение «SwitchedOff», вы можете инвертировать его, поэтому он рассчитывает мощность на ложь, а не на истину",
+    "pt": "use valor invertido. se o ponto de dados for 'SwitchedOff', você poderá invertê-lo, calculando a potência em falso e não em verdadeiro",
+    "nl": "gebruik omgekeerde waarde. als datapunt 'SwitchedOff' is, kun je het omkeren zodat het de power on false berekent en niet true",
+    "fr": "utilisez une valeur inversée. si le point de données est «SwitchedOff», vous pouvez l'inverser pour qu'il calcule la puissance sur faux et non sur vrai",
+    "it": "usa il valore invertito. se datapoint è 'SwitchedOff' è possibile invertire in modo che calcoli la potenza su false e non su true",
+    "es": "utilizar valor invertido si el punto de datos está 'apagado', puede invertirlo para que calcule la potencia en falso y no en verdadero",
+    "pl": "użyj odwróconej wartości. jeśli datapoint ma wartość „SwitchedOff”, możesz go odwrócić, aby obliczał Moc na false, a nie na true",
+    "zh-cn": "使用倒数。如果datapoint是'SwitchedOff'，则可以将其取反，从而计算false而不是true的功率"
+  }
 };
